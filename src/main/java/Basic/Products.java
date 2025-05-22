2c1f679ceaaea19f79f6cefdea9d8765b4522b70
VladMoticica <79302329+VladMoticica@users.noreply.github.com>
2025-05-22 17:44:44 +0300

    Products & Discounts G&S created

---
+++ src/main/java/Basic/Products.java
@@ -0,0 +1,76 @@
+package Basic;
+
+public class Products {
+    private String P_product_id;
+    private String P_product_name;
+    private String P_product_category;
+    private String P_brand;
+    private Float P_package_quantity;
+    private String P_package_unit;
+    private Float P_price;
+    private String P_currency;
+
+    public String getP_product_id() {
+        return P_product_id;
+    }
+
+    public void setP_product_id(String p_product_id) {
+        P_product_id = p_product_id;
+    }
+
+    public String getP_product_name() {
+        return P_product_name;
+    }
+
+    public void setP_product_name(String p_product_name) {
+        P_product_name = p_product_name;
+    }
+
+    public String getP_product_category() {
+        return P_product_category;
+    }
+
+    public void setP_product_category(String p_product_category) {
+        P_product_category = p_product_category;
+    }
+
+    public String getP_brand() {
+        return P_brand;
+    }
+
+    public void setP_brand(String p_brand) {
+        P_brand = p_brand;
+    }
+
+    public Float getP_package_quantity() {
+        return P_package_quantity;
+    }
+
+    public void setP_package_quantity(Float p_package_quantity) {
+        P_package_quantity = p_package_quantity;
+    }
+
+    public String getP_package_unit() {
+        return P_package_unit;
+    }
+
+    public void setP_package_unit(String p_package_unit) {
+        P_package_unit = p_package_unit;
+    }
+
+    public Float getP_price() {
+        return P_price;
+    }
+
+    public void setP_price(Float p_price) {
+        P_price = p_price;
+    }
+
+    public String getP_currency() {
+        return P_currency;
+    }
+
+    public void setP_currency(String p_currency) {
+        P_currency = p_currency;
+    }
+}
