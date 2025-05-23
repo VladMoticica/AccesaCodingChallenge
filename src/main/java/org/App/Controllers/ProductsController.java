e00a894f7041f3f800555b05cce20e752c74bd1a
VladMoticica <79302329+VladMoticica@users.noreply.github.com>
2025-05-23 19:06:27 +0300

    Split "Spring Boot Controller Part 1" commit into Products Controller & Discounts Controller.
    Products Controller Part 2

---
+++ src/main/java/org/App/Controllers/ProductsController.java
@@ -0,0 +1,41 @@
+package org.App.Controllers;
+
+import org.App.Basic.Products;
+import org.App.Service.ProductsService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+    private final ProductsService serviceP;
+
+    public ProductsController(ProductsService service) {
+        this.serviceP = service;
+    }
+
+    @GetMapping
+    public List<Products> getAllProducts() {
+        return serviceP.getAllProducts();
+    }
+
+    @GetMapping("/p_store/{store}")
+    public List<Products> getByStore(@PathVariable String store) {
+        return serviceP.getProductsByStore(store);
+    }
+
+    @GetMapping("/categories")
+    public Set<String> getAllCategories() {
+        return serviceP.getAllCategories();
+    }
+
+    @GetMapping("/category/{category}")
+    public List<Products> getByCategory(@PathVariable String category) {
+        return serviceP.getProductsByCategory(category);
+    }
+}
