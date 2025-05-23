0721c81c8c59df76d0755c8d507e0c3061c1a464
VladMoticica <79302329+VladMoticica@users.noreply.github.com>
2025-05-23 19:01:55 +0300

    Added JSON ordering to reflect the CSV files order, and renamed variables to lowercase

--- src/main/java/org/App/Basic/Discounts.java
+++ src/main/java/org/App/Basic/Discounts.java
@@ -1,88 +1,101 @@
 package org.App.Basic;
 
-import java.util.Date;
+import java.time.LocalDate;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({
+        "d_product_id",
+        "d_product_name",
+        "d_brand",
+        "d_package_quantity",
+        "d_package_unit",
+        "d_product_category",
+        "d_from_date",
+        "d_to_date",
+        "d_percentage_of_discount"
+})
 
 public class Discounts
 {
-    private String D_product_id;
-    private String D_product_name;
-    private String D_brand;
-    private Float D_package_quantity;
-    private String D_package_unit;
-    private String D_product_category;
-    private java.util.Date D_from_date;
-    private java.util.Date D_to_date;
-    private int D_percentage_of_discount;
+    private String d_product_id;
+    private String d_product_name;
+    private String d_brand;
+    private Float d_package_quantity;
+    private String d_package_unit;
+    private String d_product_category;
+    private LocalDate d_from_date;
+    private LocalDate d_to_date;
+    private int d_percentage_of_discount;
 
     public String getD_product_id() {
-        return D_product_id;
+        return d_product_id;
     }
 
     public void setD_product_id(String d_product_id) {
-        D_product_id = d_product_id;
+        this.d_product_id = d_product_id;
     }
 
     public String getD_product_name() {
-        return D_product_name;
+        return d_product_name;
     }
 
     public void setD_product_name(String d_product_name) {
-        D_product_name = d_product_name;
+        this.d_product_name = d_product_name;
     }
 
     public String getD_brand() {
-        return D_brand;
+        return d_brand;
     }
 
     public void setD_brand(String d_brand) {
-        D_brand = d_brand;
+        this.d_brand = d_brand;
     }
 
     public Float getD_package_quantity() {
-        return D_package_quantity;
+        return d_package_quantity;
     }
 
     public void setD_package_quantity(Float d_package_quantity) {
-        D_package_quantity = d_package_quantity;
+        this.d_package_quantity = d_package_quantity;
     }
 
     public String getD_package_unit() {
-        return D_package_unit;
+        return d_package_unit;
     }
 
     public void setD_package_unit(String d_package_unit) {
-        D_package_unit = d_package_unit;
+        this.d_package_unit = d_package_unit;
     }
 
     public String getD_product_category() {
-        return D_product_category;
+        return d_product_category;
     }
 
     public void setD_product_category(String d_product_category) {
-        D_product_category = d_product_category;
+        this.d_product_category = d_product_category;
     }
 
-    public Date getD_from_date() {
-        return D_from_date;
+    public LocalDate getD_from_date() {
+        return d_from_date;
     }
 
-    public void setD_from_date(Date d_from_date) {
-        D_from_date = d_from_date;
+    public void setD_from_date(LocalDate d_from_date) {
+        this.d_from_date = d_from_date;
     }
 
-    public Date getD_to_date() {
-        return D_to_date;
+    public LocalDate getD_to_date() {
+        return d_to_date;
     }
 
-    public void setD_to_date(Date d_to_date) {
-        D_to_date = d_to_date;
+    public void setD_to_date(LocalDate d_to_date) {
+        this.d_to_date = d_to_date;
     }
 
     public int getD_percentage_of_discount() {
-        return D_percentage_of_discount;
+        return d_percentage_of_discount;
     }
 
     public void setD_percentage_of_discount(int d_percentage_of_discount) {
-        D_percentage_of_discount = d_percentage_of_discount;
+        this.d_percentage_of_discount = d_percentage_of_discount;
     }
 }
--- src/main/java/org/App/Basic/Products.java
+++ src/main/java/org/App/Basic/Products.java
@@ -1,77 +1,90 @@
 package org.App.Basic;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({
+        "p_product_id",
+        "p_product_name",
+        "p_product_category",
+        "p_brand",
+        "p_package_quantity",
+        "p_package_unit",
+        "p_price",
+        "p_currency"
+})
+
 public class Products
 {
-    private String P_product_id;
-    private String P_product_name;
-    private String P_product_category;
-    private String P_brand;
-    private Float P_package_quantity;
-    private String P_package_unit;
-    private Float P_price;
-    private String P_currency;
+    private String p_product_id;
+    private String p_product_name;
+    private String p_product_category;
+    private String p_brand;
+    private Float p_package_quantity;
+    private String p_package_unit;
+    private Float p_price;
+    private String p_currency;
 
     public String getP_product_id() {
-        return P_product_id;
+        return p_product_id;
     }
 
     public void setP_product_id(String p_product_id) {
-        P_product_id = p_product_id;
+        this.p_product_id = p_product_id;
     }
 
     public String getP_product_name() {
-        return P_product_name;
+        return p_product_name;
     }
 
     public void setP_product_name(String p_product_name) {
-        P_product_name = p_product_name;
+        this.p_product_name = p_product_name;
     }
 
     public String getP_product_category() {
-        return P_product_category;
+        return p_product_category;
     }
 
     public void setP_product_category(String p_product_category) {
-        P_product_category = p_product_category;
+        this.p_product_category = p_product_category;
     }
 
     public String getP_brand() {
-        return P_brand;
+        return p_brand;
     }
 
     public void setP_brand(String p_brand) {
-        P_brand = p_brand;
+        this.p_brand = p_brand;
     }
 
     public Float getP_package_quantity() {
-        return P_package_quantity;
+        return p_package_quantity;
     }
 
     public void setP_package_quantity(Float p_package_quantity) {
-        P_package_quantity = p_package_quantity;
+        this.p_package_quantity = p_package_quantity;
     }
 
     public String getP_package_unit() {
-        return P_package_unit;
+        return p_package_unit;
     }
 
     public void setP_package_unit(String p_package_unit) {
-        P_package_unit = p_package_unit;
+        this.p_package_unit = p_package_unit;
     }
 
     public Float getP_price() {
-        return P_price;
+        return p_price;
     }
 
     public void setP_price(Float p_price) {
-        P_price = p_price;
+        this.p_price = p_price;
     }
 
     public String getP_currency() {
-        return P_currency;
+        return p_currency;
     }
 
     public void setP_currency(String p_currency) {
-        P_currency = p_currency;
+        this.p_currency = p_currency;
     }
 }
