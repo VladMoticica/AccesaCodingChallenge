d11bd7df277e821627796e8274af62371829d8a2
VladMoticica <79302329+VladMoticica@users.noreply.github.com>
2025-05-23 19:10:47 +0300

    Discounts Controller Part 1

---
+++ src/main/java/org/App/Controllers/DiscountsController.java
@@ -0,0 +1,40 @@
+package org.App.Controllers;
+
+import org.App.Basic.Discounts;
+import org.App.Service.DiscountsService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/discounts")
+public class DiscountsController {
+    private final DiscountsService serviceD;
+
+    public DiscountsController(DiscountsService serviceD) {
+        this.serviceD = serviceD;
+    }
+
+    @GetMapping
+    public List<Discounts> getAllDiscounts() {
+        return serviceD.getAllDiscounts();
+    }
+
+    @GetMapping("/deals")
+    public List<Discounts> getBestDeals() {
+        return serviceD.findBestDeals();
+    }
+
+    @GetMapping("/new")
+    public List<Discounts> getNewDiscounts() {
+        return serviceD.findNewDiscounts();
+    }
+
+    @GetMapping("/d_store/{store}")
+    public List<Discounts> getDByStore(@PathVariable String store) {
+        return serviceD.getDiscountsByStore(store);
+    }
+}
